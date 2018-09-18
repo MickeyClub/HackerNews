e692cae9a0283e48123fe0dd8c29f6a51471ac40
MickeyClub <972207518@qq.com>
2018-09-18 19:55:24 +0800

    搭建基本服务器完成

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require("http")
+const fs = require("fs")
+const path = require("path")
+
+http.createServer().on("request", (err, data) => {
+
+}).listen(80,() => {console.log("服务器启动中");})
